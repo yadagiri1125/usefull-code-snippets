663e288a63621a2e484b63383aaf15563b11cb26
yadagiri <yadagiri1125@gmail.com>
2014-01-23 10:59:13 +0530

    adds a command in readme file by which we can convert all erb file into haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railscasts::Application.config.secret_token = '3b81c5337dffd93b318c53c6a2d333f101017aabc3b75c102f906bdf87b0152e96ab97ec1895e483791cd83908b1dda7d8bb5b31e3b085ab5c0587edf33f9765'
